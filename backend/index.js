ae8a53ee2e14457066498aeb1d93e7f459224e73
Shailesh Dadhich <kesh4v50@gmail.com>
2026-09-15 20:42:43 +0530

    feat: add MongoDB client connection logic

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+const dns = require("dns");
 require("dotenv").config();
 
-const app = express()
+dns.setServers(["1.1.1.1", "1.0.0.1"]);
+
+const connectDB = require("./config/db");
+connectDB();
+
+const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("PrepCraft API running");
+  res.send("PrepCraft API running");
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=> {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
